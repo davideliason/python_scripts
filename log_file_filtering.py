e10fc5a9f2005bce3db03920bda9461a388cb02b
davideliason <davethemaker@gmail.com>
2022-05-21 02:19:40 -0700

    filter log files with 'info' within

--- log_file_filtering.py
+++ log_file_filtering.py
@@ -3,6 +3,8 @@
 import sys
 logfile = sys.argv[1]
 
-with open(logfile) as f:
+with open(logfile) as f: # returns a file object that can be acted upon
     for line in f:
-	    print(line.strip())
+    	if "INFO" not in line: # filter out only logs with this in the line
+    		continue
+    	print(line.strip())
