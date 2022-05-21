2213e0a04bfdf23c7c0b048b4ea54adc5a1af421
davideliason <davethemaker@gmail.com>
2022-05-21 07:05:37 -0700

    use regex expression to filter the log commands to stdout specific

--- log_file_filtering.py
+++ log_file_filtering.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
 logfile = sys.argv[1]
 
 with open(logfile) as f: # returns a file object that can be acted upon
     for line in f:
     	if "INFO" not in line: # filter out only logs with this in the line
     		continue
+    	pattern = r'\d{4}\w*\d{6} .*)'
     	print(line.strip())
