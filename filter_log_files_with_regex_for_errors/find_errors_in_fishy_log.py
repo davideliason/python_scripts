7e8a9eec34dc01c78ac3580376133794507a94d7
davideliason <davethemaker@gmail.com>
2022-05-22 14:27:10 -0700

    Script with two functions: first to open given file, search for terms as given by user using regex, and second function to save matching log quieries to output file

--- filter_log_files_with_regex_for_errors/find_errors_in_fishy_log.py
+++ filter_log_files_with_regex_for_errors/find_errors_in_fishy_log.py
@@ -8,20 +8,21 @@ import os
 import re
 
 def error_search(log_file):
-    error = input("What is the error? ")
-    returned_errors = []
-    with open(log_file, mode='r',encoding='UTF-8') as file:
-        for log in file.readlines():
-            error_patterns = ["error"]
+    error = input("What is the error? ")  # user input
+    returned_errors = [] # matching logs that meet user specified criteria
+    with open(log_file, mode='r',encoding='UTF-8') as file:  #open the file
+        for log in file.readlines():  # read through the file line by line
+            error_patterns = ["error"] # the errors that the user want to search for
             for i in range(len(error.split(' '))):
-                error_patterns.append(r"{}".format(error.split(' ')[i].lower()))
+                error_patterns.append(r"{}".format(error.split(' ')[i].lower())) # the error search terms
             if all(re.search(error_pattern, log.lower()) for error_pattern in error_patterns):
                 returned_errors.append(log)
         file.close()
     return returned_errors
   
 def file_output(returned_errors):
-    with open('errors_found.log', 'w') as file:
+    with open(os.path.expanduser('~') + '/Documents/python/python_scripts/errors_found.log', 'w') as file: # we use the os.path.expanduser('~') which returns the home directory of my system instance
+    	# then concatenate to the file for output
         for error in returned_errors:
             file.write(error)
         file.close()
